73ba883755ce3e8db04b1d2a67b79c425b7d3572
suhail <suhail518650@gmail.com>
2023-07-11 23:09:10 +0530

    Task1

---
+++ task1.js
@@ -0,0 +1,28 @@
+const readlineSync = require('readline-sync');
+const num = parseInt(readlineSync.question(`Enter a number between 1 to 7 : `));
+switch (num) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalide Entry");
+        break;
+}
